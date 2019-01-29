2d417db7fee0cbd6d080481de4a1a628b695d2fd
Marinette <marinette.m.chen@gmail.com>
2019-01-28 20:31:48 -0500

    Implemented MSE, gradMSE, grad_descent
    it probably works

--- LAB1/a1/starter.py
+++ LAB1/a1/starter.py
@@ -19,23 +19,58 @@ def loadData():
         trainData, trainTarget = Data[:3500], Target[:3500]
         validData, validTarget = Data[3500:3600], Target[3500:3600]
         testData, testTarget = Data[3600:], Target[3600:]
+        print("Success")
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
+    # for training: W^T must be a 28x28 weight matrix
+    # x[i] = 28x28 (picture)
 def MSE(W, b, x, y, reg):
-    # Your implementation here
+    mse = 0
+    for i in range(len(y)):
+        mse+=(1/(2*len(y)))*(W.flatten().dot(np.transpose(x[i].flatten()))+b-y[i])**2 + reg/2*np.linalg.norm(W)**2
+              
+    return mse
 
 def gradMSE(W, b, x, y, reg):
-    # Your implementation here
-
+    #calculate gradient with respect to W
+    gradW=0
+    gradB=0
+    for i in range(len(y)):
+        gradW += (1/len(y))*((W.flatten().dot(x[i].flatten().transpose())+b-y[i])*x[i]) + reg*W
+        gradB += 1/len(y)*(W.flatten().dot(x[i].flatten().transpose())+b-y[i])
+    #calculate gradient with respect to regularization-tor
+    return gradW, gradB
+    
 def crossEntropyLoss(W, b, x, y, reg):
-    # Your implementation here
+    pass
 
 def gradCE(W, b, x, y, reg):
-    # Your implementation here
+    pass
 
+# While the error is above threshold, keep updating weight matrix
 def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS):
-    # Your implementation here
+    
+    error = MSE(W,b,trainingData,trainingLabels,reg)
+    while( error > EPS):
+        # idk if this syntax is right gotta go check later
+        print(error)
+        gradW, gradB = gradMSE(W,b,trainingData,trainingLabels,reg)
+        W = np.subtract(W,alpha*gradW)
+        b = b - alpha*gradB
+        error = MSE(W,b,trainingData,trainingLabels,reg)
+    return W,b
 
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
-    # Your implementation here
+    pass
 
+def main():
+    trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    W = np.ones(shape=(28,28))
+    b = 0
+    W, b = grad_descent(W,b,trainData, trainTarget, 0.001, 5000, 0, 10**(-7))
+    print(W)
+    print(b)
+    a = np.matrix([[1,2],[3,4]])
+    
+if __name__ == "__main__":
+    main()
